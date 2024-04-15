05145fae0c8ea8537f4c9154fb7d2a513b9a0fab
woosanghansangwoo <woo94835538@gmail.com>
2024-04-15 20:45:02 +0900

    집가서 마저하기

--- lib/widgets/AppBarWidget.dart
+++ lib/widgets/AppBarWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testapp/utils/AppColors.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   const AppBarWidget({super.key});
@@ -10,6 +11,8 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.white,
+      elevation: 0,
+      foregroundColor: AppColors.mainColorTest,
       // 기타 AppBar 속성 추가
     );
   }
