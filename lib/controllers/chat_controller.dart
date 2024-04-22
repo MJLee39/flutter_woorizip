17d2b7768c93e457ba3a54495cf47257db283093
BullChallenger <am23am45@naver.com>
2024-04-22 17:51:08 +0900

    [FIX] 채팅 역순 정렬 나는 버그 해결

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -47,6 +47,20 @@ class ChatController {
       throw Exception('Failed to delete exit chatRoom');
     }
   }
+
+  Future<dynamic> fetchChatRoom(String chatRoomId, String accountId) async {
+    final url = Uri.parse("https://chat.teamwaf.app/chat/room/resp?chatRoomId=$chatRoomId&accountId=$accountId");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(utf8.decode(response.bodyBytes));
+      final chatMessages = jsonResponse['chatMessagesInRoom'];
+      return chatMessages;
+    } else {
+      throw Exception("Failed to fetch chat room");
+    }
+  }
+
 }
 
 class ChatRoomResponseDTO {
